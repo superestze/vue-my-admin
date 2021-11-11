47946108aa1b5ebdf7f5e81405598bf193e76dca
zehui <lizh285@media.com>
2021-11-11 15:06:51 +0800

    feat: i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import zhLocal from './lang/zh'
 import enLocal from './lang/en'
+import store from '@/store'
 const messages = {
   en: {
     msg: {
@@ -14,14 +15,16 @@ const messages = {
   }
 }
 
-const locale = 'zh'
+function getLanguage() {
+  return store && store.getters && store.getters.language
+}
 
 const i18n = createI18n({
   // 使用 Composition API 模式，则需要将其设置为false
   legacy: false,
   // 全局注入 $t 函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
 
